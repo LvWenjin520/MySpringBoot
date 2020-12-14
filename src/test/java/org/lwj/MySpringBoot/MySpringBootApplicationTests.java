b102b7f95fb9909e2d64d66eb14ed6f0711e5e37
lwj <972950811@qq.com>
2020-12-14 14:53:08 +0800

    第一次上传springboot项目

---
+++ src/test/java/org/lwj/MySpringBoot/MySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lwj.MySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
