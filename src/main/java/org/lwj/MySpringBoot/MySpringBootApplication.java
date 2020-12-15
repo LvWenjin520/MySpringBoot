179353e0eff041082f72e7b7558975516477b66e
lwj <972950811@qq.com>
2020-12-15 09:05:10 +0800

    提交修改

--- src/main/java/org/lwj/MySpringBoot/MySpringBootApplication.java
+++ src/main/java/org/lwj/MySpringBoot/MySpringBootApplication.java
@@ -3,6 +3,7 @@ package org.lwj.MySpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//程序的主入口，不能删不能改,本身就是spring的组件
 @SpringBootApplication
 public class MySpringBootApplication {
 
