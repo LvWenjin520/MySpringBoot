5bba001a361fbb205eb63458978f6295aca29a78
LvWenJin <972950811@qq.com>
2020-12-22 22:34:43 +0800

    页面跳转测试templates文件夹测试

---
+++ src/main/java/org/lwj/MySpringBoot/controller/TestController.java
@@ -0,0 +1,12 @@
+package org.lwj.MySpringBoot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+	@RequestMapping("/test")
+	public String test() {
+		return "test01";
+	}
+}
